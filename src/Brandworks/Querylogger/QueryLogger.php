bdad5cc68dc16347fb90c485e27022fb31d27c76
Hans VN <hans@brandworks.be>
2015-10-02 11:48:58 +0200

    added writing to log file

--- src/Brandworks/Querylogger/QueryLogger.php
+++ src/Brandworks/Querylogger/QueryLogger.php
@@ -2,6 +2,8 @@
 
 use DB;
 use Config;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 class QueryLogger {
 	/**
@@ -19,17 +21,61 @@ class QueryLogger {
 	protected $loggingQueries = true;
 
 	/**
+	 * Indicates whether queries logs are written to file.
+	 *
+	 * @var bool
+	 */
+	protected $writingQueries = true;
+
+	/**
+	 * Indicates the file location of the query logs.
+	 *
+	 * @var String
+	 */
+	protected $logFile = 'logs/queries/queryLog.log';
+
+	/**
+	 * Indicates the file location of the query logs.
+	 *
+	 * @var bool
+	 */
+	protected $fillQueries = true;
+
+	/**
+	 * Indicates the file location of the query logs.
+	 *
+	 * @var Logger
+	 */
+	private $fileLogger = null;
+
+	/**
 	 * Create a new logger instance.
 	 *
 	 * @return void
 	 */
 	public function __construct() {
-		if(Config::get('querylogger::disable_laravel_log')) {
+		if(Config::get('querylogger::disable_laravel_log') === true) {
 			DB::disableQueryLog();
 		}
-		
-		if(Config::get('querylogger::enabled')) {
+
+		if(Config::get('querylogger::enabled') === false) {
 			$this->loggingQueries = false;
+		}
+
+		if(Config::get('querylogger::logFile') && Config::get('querylogger::logFile') != "") {
+			$this->logFile = Config::get('querylogger::logFile');
+		}
+		$this->logFile = storage_path($this->logFile);
+
+		if(Config::get('querylogger::fill_queries') === false) {
+			$this->fillQueries = false;
+		}
+
+		if(Config::get('querylogger::write_queries') === false) {
+			$this->writingQueries = false;
+		} else {
+			$this->fileLogger = new Logger('Query Logs');
+			$this->fileLogger->pushHandler(new StreamHandler($this->logFile, Logger::INFO));
 		}
 	}
 
@@ -45,7 +91,28 @@ class QueryLogger {
 	public function logQuery($query, $bindings, $called_at, $time = null) {
 		if ( ! $this->loggingQueries) return;
 
-		$this->queryLog[] = compact('query', 'bindings', 'called_at', 'time');
+		$this->queryLog[] = $query_data = compact('query', 'bindings', 'called_at', 'time');
+
+		//write the queries
+		if($this->writingQueries && $this->fileLogger != null) {
+			if($this->fillQueries) {
+				// format the bindings for insertion
+				foreach ($bindings as $key => $binding) {
+					if ($binding instanceof \DateTime) {
+						$bindings[$key] = $binding->format('\'Y-m-d H:i:s\'');
+					} else if (is_string($binding)) {
+						$bindings[$key] = "'$binding'";
+					}
+				}
+
+				// insert bindings into query
+				$query = str_replace(array('%', '?'), array('%%', '%s'), $query);
+				$query = vsprintf($query, $bindings);
+			}
+
+        	// add the record to the query log
+        	$this->fileLogger->addInfo($query, $query_data);
+		}
 	}
 
 	/**
@@ -62,8 +129,7 @@ class QueryLogger {
 	 *
 	 * @return void
 	 */
-	public function flushQueryLog()
-	{
+	public function flushQueryLog() {
 		$this->queryLog = array();
 	}
 
@@ -72,8 +138,7 @@ class QueryLogger {
 	 *
 	 * @return void
 	 */
-	public function enableQueryLog()
-	{
+	public function enableQueryLog() {
 		$this->loggingQueries = true;
 	}
 
@@ -82,8 +147,7 @@ class QueryLogger {
 	 *
 	 * @return void
 	 */
-	public function disableQueryLog()
-	{
+	public function disableQueryLog() {
 		$this->loggingQueries = false;
 	}
 
@@ -92,8 +156,88 @@ class QueryLogger {
 	 *
 	 * @return bool
 	 */
-	public function logging()
-	{
+	public function logging() {
 		return $this->loggingQueries;
+	}
+
+	/**
+	 * Enable the writing of the queries.
+	 *
+	 * @return void
+	 */
+	public function enableWriting() {
+		$this->writingQueries = true;
+	}
+
+	/**
+	 * Disable the writing of the queries.
+	 *
+	 * @return void
+	 */
+	public function disableWriting() {
+		$this->writingQueries = false;
+	}
+
+	/**
+	 * Determine whether we're writing queries.
+	 *
+	 * @return bool
+	 */
+	public function writing() {
+		return $this->writingQueries;
+	}
+
+	/**
+	 * Set the log file
+	 *
+	 * @return void
+	 */
+	public function setLogFile($log_file) {
+		$this->logFile = storage_path($log_file);
+
+		//reset the writing to log file to the new location
+		if($this->writingQueries) {
+			$this->fileLogger = new Logger('Query Logs');
+			$this->fileLogger->pushHandler(new StreamHandler($this->logFile, Logger::INFO));
+		} else {
+			$this->fileLogger = null;
+		}
+	}
+
+	/**
+	 * Get the log file.
+	 *
+	 * @return void
+	 */
+	public function logFile() {
+		//return the original log file - not the full path
+		return str_replace(storage_path(), "", $this->logFile);
+	}
+
+	/**
+	 * Enable the filling of the queries.
+	 *
+	 * @return void
+	 */
+	public function enableFilling() {
+		$this->fillQueries = true;
+	}
+
+	/**
+	 * Disable the filling of the queries.
+	 *
+	 * @return void
+	 */
+	public function disableFilling() {
+		$this->fillQueries = false;
+	}
+
+	/**
+	 * Determine whether we're filling queries with the bindings.
+	 *
+	 * @return bool
+	 */
+	public function filling() {
+		return $this->fillQueries;
 	}
 }
